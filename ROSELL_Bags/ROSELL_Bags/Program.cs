417db246452f7713053c9f8ad13ecbea70b87f95
IAMDOGG <124060531+IAMDOGG@users.noreply.github.com>
2023-03-06 01:08:52 +0800

    Add files via upload

---
+++ ROSELL_Bags/ROSELL_Bags/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROSELL_Bags
+{
+    internal class Bags
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Number of Bags Ordered: ");
+            int numBags = Convert.ToInt32(Console.ReadLine());
+
+            int numLargeBoxes = numBags / 20;
+            
+            int numMediumBoxes = (numBags % 20) / 10;
+
+            int numSmallBoxes = (numBags % 10 + 4) / 5;
+
+
+            double totalCost = numLargeBoxes * 3 +
+                               numMediumBoxes * 2.5 +
+                               numSmallBoxes * 2 +
+                               numBags * 5.5;
+
+            Console.WriteLine("Boxes Used:");
+            Console.WriteLine($"Large - {numLargeBoxes}");
+            Console.WriteLine($"Medium - {numMediumBoxes}");
+            Console.WriteLine($"Small - {numSmallBoxes}");
+            Console.WriteLine($"Your total cost is: ${totalCost:F2}");
+        }
+    }
+}
